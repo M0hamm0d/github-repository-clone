861e6996c37c89d1f28541703888513e788225fd
Muhammad Abdussalam <masalami042@gmail.com>
2024-09-15 23:07:36 +0100

    Added new svg files and also worked on some of the dropdowns on the page

--- index.js
+++ index.js
@@ -37,35 +37,11 @@ async function repoEndPoint() {
                 <div class="">
                   <button class="stars">
                     <div class="">
-                      <svg
-                        aria-hidden="true"
-                        height="16"
-                        viewBox="0 0 16 16"
-                        version="1.1"
-                        width="16"
-                        data-view-component="true"
-                        class="octicon octicon-star d-inline-block mr-2"
-                      >
-                        <path
-                          d="M8 .25a.75.75 0 0 1 .673.418l1.882 3.815 4.21.612a.75.75 0 0 1 .416 1.279l-3.046 2.97.719 4.192a.751.751 0 0 1-1.088.791L8 12.347l-3.766 1.98a.75.75 0 0 1-1.088-.79l.72-4.194L.818 6.374a.75.75 0 0 1 .416-1.28l4.21-.611L7.327.668A.75.75 0 0 1 8 .25Zm0 2.445L6.615 5.5a.75.75 0 0 1-.564.41l-3.097.45 2.24 2.184a.75.75 0 0 1 .216.664l-.528 3.084 2.769-1.456a.75.75 0 0 1 .698 0l2.77 1.456-.53-3.084a.75.75 0 0 1 .216-.664l2.24-2.183-3.096-.45a.75.75 0 0 1-.564-.41L8 2.694Z"
-                        ></path>
-                      </svg>
-                      <p>star</p>
+                      <img src="asset/images/svg/star.svg" alt="" />
+                      <p>Star</p>
                     </div>
                     <div class="">
-                      <svg
-                        aria-hidden="true"
-                        height="16"
-                        viewBox="0 0 16 16"
-                        version="1.1"
-                        width="16"
-                        data-view-component="true"
-                        class="octicon octicon-triangle-down"
-                      >
-                        <path
-                          d="m4.427 7.427 3.396 3.396a.25.25 0 0 0 .354 0l3.396-3.396A.25.25 0 0 0 11.396 7H4.604a.25.25 0 0 0-.177.427Z"
-                        ></path>
-                      </svg>
+                      <img src="asset/images/svg/dropdown.svg" alt="" />
                     </div>
                   </button>
                 </div>
